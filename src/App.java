775dace885c839e52d688b02a4e4ee88a809ead7
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-07-24 15:11:26 -0300

    feat: Implementcao basica curso

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.time.LocalDate;
+
+import bootcamp.model.Curso;
+import bootcamp.model.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // criando cursos
+        Curso curso1 = new Curso("Java", "Curso de Java", 40);
+        Curso curso2 = new Curso("Python", "Curso de Python", 50);
+
+        // exibindo os cursos
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+
+        // criando mentorias
+        Mentoria mentoria1 = new Mentoria("Java", "Mentoria de Java", LocalDate.now());
+        Mentoria mentoria2 = new Mentoria("Python", "Mentoria de Python", LocalDate.now());
+
+        // exibindo as mentorias
+        System.out.println(mentoria1);
+        System.out.println(mentoria2);
     }
 }
