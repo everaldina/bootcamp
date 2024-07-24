1c04db48a28eff8248924187b39ad2c553cc6b9d
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-07-24 15:29:31 -0300

    feat: logica inical bootcamp

--- src/bootcamp/model/Bootcamp.java
+++ src/bootcamp/model/Bootcamp.java
@@ -1,11 +1,78 @@
 package bootcamp.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Bootcamp {
     private String nome;
     private String descricao;
-    private LocalDate inicio;
-    private LocalDate fim;
+    private final LocalDate inicio;
+    private final LocalDate fim;
+    private Set<Dev> devsInscritos;
+    private Set<Conteudo> conteudos;
+
+
+    public Bootcamp(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.inicio = LocalDate.now();
+        this.fim = inicio.plusDays(45);
+        this.conteudos = new LinkedHashSet<>();
+        this.devsInscritos = new HashSet<>();
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+    public LocalDate getInicio() {
+        return inicio;
+    }
+
+
+    public LocalDate getFim() {
+        return fim;
+    }
+
+
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
+    }
+
+
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }
+
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    
 
 }
