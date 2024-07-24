64c43ecc7a7e1a79c6ea2099fe3b0ed928a45902
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-07-24 15:26:13 -0300

    patch: logica inicial dev

--- src/bootcamp/model/Conteudo.java
+++ src/bootcamp/model/Conteudo.java
@@ -34,5 +34,38 @@ public abstract class Conteudo {
         this.descricao = descricao;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Conteudo other = (Conteudo) obj;
+        if (titulo == null) {
+            if (other.titulo != null)
+                return false;
+        } else if (!titulo.equals(other.titulo))
+            return false;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        return true;
+    }
+
+    
+
     
 }
--- src/bootcamp/model/Dev.java
+++ src/bootcamp/model/Dev.java
@@ -1,24 +1,56 @@
 package bootcamp.model;
 
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Dev {
     private String nome;
-    private List<Mentoria> mentorias;
-    private List<Curso> atividades;
+    private Set<Conteudo> conteudosInscritos;
+    private Set<Conteudo> conteudosConcluidos;
 
+
+    public Dev(String nome) {
+        this.nome = nome;
+        this.conteudosInscritos = new LinkedHashSet<>();
+        this.conteudosConcluidos = new LinkedHashSet<>();
+    }
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
     }
 
-    public void progredir() {
+
+    public void progredir(Conteudo conteudo) {
+        
     }
 
-    public double calcularTotalXp() {
-        return 0;
+    public void calcularTotalXp(){
+
     }
 
-    public void exibirCursos() {
+    public String getNome() {
+        return nome;
     }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+
 
 }
